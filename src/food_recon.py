8979c1f3b8b5679d2f6dd65818b1b8849fa0e437
Gustavo Gomez Lopez De San Roman <gustavogomezlopezdesanroman@gmail.com>
2020-05-08 20:49:39 +0200

    Added Logic space of ingredients

---
+++ src/food_recon.py
@@ -0,0 +1,9 @@
+from clarifai.rest import ClarifaiApp
+import json
+
+
+def predict_photo(photo):
+    app = ClarifaiApp(api_key='e1f4f037491d42d298b6b7c80ee778e4')
+    model = app.public_models.general_model
+    response = model.predict_by_bytes(photo)
+    return response["outputs"][0]["data"]["concepts"][0]["name"]
--- src/ingredients_Adapter.py
+++ src/ingredients_Adapter.py
@@ -1,6 +1,62 @@
+import emoji
 from chatterbot.conversation import Statement
 from chatterbot.comparisons import JaccardSimilarity
 from chatterbot.logic import LogicAdapter
+from telebot.types import InlineKeyboardMarkup
+from telegram import InlineKeyboardButton
+
+
+class ingredientChosser(object):
+    def __init__(self, **kwargs):
+        pass
+
+    @staticmethod
+    def can_process(statement):
+        # TODO(DATABASE) CALL DATABASE TO SEE IF I AM IN THE CORRECT STATE FOR NOW, SEARCH WORD INGREDIENT
+        lower_string = statement.text
+        if "ingredient" in lower_string.lower():
+            return True
+        return False
+
+    @staticmethod
+    def process(statement):
+        return JaccardSimilarity().compare(Statement(statement.text), Statement("ingredient"))
+
+    @staticmethod
+    def response(statement, bot):
+        # change state of user
+        markup = InlineKeyboardMarkup()
+        markup.row_width = 2
+        markup.add(InlineKeyboardButton("Add ingredient", callback_data="add_ingredient"),
+                   InlineKeyboardButton("List ingredient", callback_data="list_ingredient"))
+        bot.send_message(statement.id, "What do you want to do", reply_markup=markup)
+
+
+class listIngredient(object):
+    def __init__(self, **kwargs):
+        pass
+
+    @staticmethod
+    def can_process(statement):
+        # TODO(DATABASE) CALL DATABASE TO SEE IF I AM IN THE CORRECT STATE FOR NOW, SEARCH WORD INGREDIENT
+        lower_string = statement.text
+        if "ingredient" in lower_string.lower():
+            return True
+        return False
+
+    @staticmethod
+    def process(statement):
+        return JaccardSimilarity().compare(Statement(statement.text), Statement("list ingredient"))
+
+    @staticmethod
+    def response(statement, bot):
+        # get all the information of the database
+        ingredients = list()
+        for ingredient in ingredients:
+            bot.send_message(statement.id, print(ingredient))
+        else:
+            bot.send_message(statement.id, "Seems like you have any ingredients")
+        # change state of user
 
 
 class addIngredient(object):
@@ -21,4 +77,55 @@ class addIngredient(object):
 
     @staticmethod
     def response(statement, bot):
-        bot.send_message(statement.id, "This is Chefbot")
+        bot.send_message(statement.id, "Great")
+        bot.send_message(statement.id, "You can take a picture, or add it manually.")
+        # change state of user
+
+
+class addIngredientNameManually(object):
+
+    @staticmethod
+    def can_process(statement):
+        # TODO(DATABASE) CALL DATABASE TO SEE IF I AM IN THE CORRECT STATE FOR NOW, SEARCH WORD INGREDIENT
+        lower_string = statement.text
+        if "add" in lower_string.lower():
+            return True
+        return False
+
+    @staticmethod
+    def process(statement):
+        return 1
+
+    @staticmethod
+    def response(statement, bot):
+        # Ask database to see if the ingredient exist
+        if True:
+            bot.send_message(statement.id, "Nice, now add the cuantity")
+            # Save pending ingredient --> to the database
+            # change state of user
+        else:
+            bot.send_message(statement.id, "This not seem like an ingredient")
+            bot.send_message(statement.id, "Could you repeat?")
+
+
+class addIngredientNameManually(object):
+
+    @staticmethod
+    def can_process(statement):
+        # TODO(DATABASE) CALL DATABASE TO SEE IF I AM IN THE CORRECT STATE FOR NOW, SEARCH WORD INGREDIENT
+        # Check the state of to add quantity of indredient
+        return True
+
+    @staticmethod
+    def process(statement):
+        return 1
+
+    @staticmethod
+    def response(statement, bot):
+        # Parse ingredients and save them to the database
+        if True:
+            bot.send_message(statement.id, emoji.emojize("Nice, item added to you list of ingredients thumbs_up:"))
+            # change state of user
+        else:
+            bot.send_message(statement.id, "This not seem like a cuantity")
+            bot.send_message(statement.id, "Could you repeat?")
